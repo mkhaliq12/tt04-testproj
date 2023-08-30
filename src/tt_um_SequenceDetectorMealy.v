2b00c89d3749461b0205252e4732aa04f5f18b06
mkhaliq12 <139871694+mkhaliq12@users.noreply.github.com>
2023-08-30 10:34:42 +0500

    Update tt_um_SequenceDetectorMealy.v

--- src/tt_um_SequenceDetectorMealy.v
+++ src/tt_um_SequenceDetectorMealy.v
@@ -4,17 +4,26 @@
 
 
 module tt_um_SequenceDetectorMealy(
-    input clk,
-    input reset, 
-    input ain,
-    output reg yout,
-    output reg[1:0] count
+    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
+    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
+    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
+    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // will go high when the design is enabled
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
     );
+
+    reg yout;
+    wire ain;
+    reg [1:0] count;
     
-    //reg[3:0] count;
+    
+    assign uo_out[0] = yout;
+    assign ain = ui_in[0];
     
     always@(posedge clk) begin
-        if (reset) begin count = 0; yout <= 1; end
+        if (rst_n) begin count = 0; yout <= 1; end
         if(ain) begin
             count = count +1;
             if (count == 3)
